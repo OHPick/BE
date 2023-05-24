51fac3c105ad939ad8e951a03ef2ad661b8dfff4
Kangjunesu <kulbit92@naver.com>
2023-05-24 16:46:28 +0900

    feat : 회원가입 passwordCheck추가 - requestMapping에 /api 추가

--- src/main/java/com/team11/shareoffice/member/dto/SignupRequestDto.java
+++ src/main/java/com/team11/shareoffice/member/dto/SignupRequestDto.java
@@ -11,11 +11,11 @@ public class SignupRequestDto {
     @NotBlank
     private String email;
 
-    @Size(min = 2, max = 10,message = "[닉네임은 2글자~10자 사이로 입력해주세요]")
+    @Size(min = 2, max = 10)
     @NotBlank
     private String nickname;
 
-    @Size(min = 4, max = 15, message = "비밀번호는 8 이상, 15 이하만 가능합니다.")
+    @Size(min = 4, max = 15)
     @NotBlank
     private String password;
 
