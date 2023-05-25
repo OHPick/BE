a1267a928c13c188342e25695623448dcaafcd2e
DESKTOP-QICCFCP\mdc <mdc53@naver.com>
2023-05-25 23:40:49 +0900

    refactor: 좋아요

--- src/main/java/com/team11/shareoffice/like/repository/LikeRepository.java
+++ src/main/java/com/team11/shareoffice/like/repository/LikeRepository.java
@@ -4,7 +4,6 @@ import com.team11.shareoffice.like.entity.Likes;
 import com.team11.shareoffice.member.entity.Member;
 import com.team11.shareoffice.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.parameters.P;
 
 public interface LikeRepository extends JpaRepository<Likes, Long> {
 
