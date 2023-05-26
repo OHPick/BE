4dd0a210c091103a668ee701f0ef7bef545b18a6
DESKTOP-QICCFCP\mdc <mdc53@naver.com>
2023-05-26 23:05:40 +0900

    feat:좋아요한 목록, 나의 게시글목록 기능 구현

--- src/main/java/com/team11/shareoffice/like/repository/LikeRepository.java
+++ src/main/java/com/team11/shareoffice/like/repository/LikeRepository.java
@@ -5,9 +5,12 @@ import com.team11.shareoffice.member.entity.Member;
 import com.team11.shareoffice.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LikeRepository extends JpaRepository<Likes, Long> {
 
     Likes findByMemberAndPost(Member member, Post post);
+    List<Likes> findAllByMemberAndLikeStatus(Member member, boolean likeStatus);
 
     void deleteLikesByPost(Post post);
 
---
+++ src/main/java/com/team11/shareoffice/post/dto/PostResponseDto.java
@@ -0,0 +1,24 @@
+package com.team11.shareoffice.post.dto;
+
+
+import com.team11.shareoffice.post.entity.Post;
+import lombok.Getter;
+
+@Getter
+public class PostResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String location;
+    private int price;
+    private boolean likeStatus;
+
+    public PostResponseDto(Post post, boolean likeStatus) {
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.location = post.getLocation();
+        this.price = post.getPrice();
+        this.likeStatus = likeStatus;
+    }
+}
