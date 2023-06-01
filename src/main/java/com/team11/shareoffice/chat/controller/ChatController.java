df4fc51990cfcfa14ad52b2c230d08e210692b73
DESKTOP-QICCFCP\mdc <mdc53@naver.com>
2023-06-01 18:03:32 +0900

    chore:rediect uri 수정

--- src/main/java/com/team11/shareoffice/chat/controller/ChatController.java
+++ src/main/java/com/team11/shareoffice/chat/controller/ChatController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 public class ChatController {
     private final ChatService chatService;
 
-
 //    @GetMapping("/chat/room")
 //    public ResponseDto findMessageHistory(@RequestParam(value = "roomId", required = false) Long roomId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
 //        return chatService.findMessageHistory(roomId, userDetails.getUser());
