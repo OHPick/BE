3ed9654796a55b8e500974e4a99d9e524f718fb0
joshWPark <parkjoshua98@gmail.com>
2023-06-06 03:00:46 +0900

    Feat : 예약완료 화면 추가

--- src/main/java/com/team11/shareoffice/reservation/service/ReservationService.java
+++ src/main/java/com/team11/shareoffice/reservation/service/ReservationService.java
@@ -2,15 +2,19 @@ package com.team11.shareoffice.reservation.service;
 
 
 import com.team11.shareoffice.global.dto.ResponseDto;
+import com.team11.shareoffice.global.security.UserDetailsImpl;
 import com.team11.shareoffice.member.entity.Member;
 import com.team11.shareoffice.post.entity.Post;
 import com.team11.shareoffice.reservation.dto.ReservationRequestDto;
+import com.team11.shareoffice.reservation.dto.ReservationResponseDto;
 import com.team11.shareoffice.reservation.entity.Reservation;
 import com.team11.shareoffice.reservation.repository.ReservationRepository;
 import com.team11.shareoffice.reservation.validator.ReservationValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +23,14 @@ public class ReservationService {
 
     private final ReservationRepository reservationRepository;
     private final ReservationValidator reservationValidator;
+
+    public ResponseDto<?> showReservedPost( Long postId, Member member) {
+        Post post = reservationValidator.validateIsExistPost(postId);
+        Reservation reservation = reservationValidator.validateReservation(post, member);
+
+        return ResponseDto.setSuccess(new ReservationResponseDto(post,reservation));
+    }
+
 
     public ResponseDto<?> reservePost(Long postId, ReservationRequestDto requestDto, Member member) {
         Post post = reservationValidator.validateIsExistPost(postId);
