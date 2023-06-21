65419aa2947fcae4b2dfaef24fc76547969a602d
Kangjunesu <kulbit92@naver.com>
2023-06-22 03:54:56 +0900

    chore : 게시글 상세조회시 유저 이미지 반영

--- src/main/java/com/team11/shareoffice/global/jwt/JwtAuthFilter.java
+++ src/main/java/com/team11/shareoffice/global/jwt/JwtAuthFilter.java
@@ -24,7 +24,6 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("JwtAuthFilter.doFilterInternal");
 
         String accessToken = jwtUtil.resolveToken(request, JwtUtil.ACCESS_TOKEN);
         String refreshToken = jwtUtil.resolveToken(request, JwtUtil.REFRESH_TOKEN);
